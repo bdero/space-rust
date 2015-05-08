6f7375ed364b34d0165bee22a8f9957790f55734
Brandon DeRosier <bdero@mit.edu>
2015-05-07 22:42:55 -0400

    Start game module

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,41 @@ extern crate sdl2;
 
 use sdl2::video::{ Window, WindowPos };
 
+
+mod sr {
+
+    extern crate sdl2;
+
+
+    pub enum Error {
+        Sdl2Error(String)
+    }
+
+    pub struct Game {
+        pub context: sdl2::sdl::Sdl
+    }
+
+    impl Game { }
+
+    pub fn init() -> Result<Game, Error> {
+        match sdl2::init(sdl2::INIT_EVERYTHING) {
+            Ok(context) => Ok(Game { context: context }),
+            Err(message) => Err(Error::Sdl2Error(
+                format!("Couldn't initialize SDL context: {}", message)))
+        }
+    }
+
+}
+
 fn main() {
     // Initialize sdl2
-    let sdl_context = match sdl2::init(sdl2::INIT_EVERYTHING) {
-        Ok(sdl_context) => sdl_context,
-        Err(error) => panic!("Couldn't initialize SDL context: {}", error)
+    let game = match sr::init() {
+        Ok(context) => context,
+        Err(error) => panic!(error)
     };
 
     // Create a window
-    let window = match Window::new(&sdl_context, "Space Rust",
+    let window = match Window::new(&game.context, "Space Rust",
                                    WindowPos::PosCentered,
                                    WindowPos::PosCentered,
                                    800, 600,
@@ -20,7 +46,7 @@ fn main() {
     };
 
     // Start the event loop
-    let mut event_pump = sdl_context.event_pump();
+    let mut event_pump = game.context.event_pump();
 
     'update: loop {
         // Poll events
